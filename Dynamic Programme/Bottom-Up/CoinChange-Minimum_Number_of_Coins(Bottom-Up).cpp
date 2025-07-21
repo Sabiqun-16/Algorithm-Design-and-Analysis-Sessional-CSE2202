c7b40067d5a44265445032ad4ef4d6a6c57e6d12
Sabiqunnahar <sabiqunnahar65@gmail.com>
2025-07-22 00:21:38 +0600

    Update CoinChange-Minimum_Number_of_Coins(Bottom-Up).cpp

--- Dynamic Programme/Bottom-Up/CoinChange-Minimum_Number_of_Coins(Bottom-Up).cpp
+++ Dynamic Programme/Bottom-Up/CoinChange-Minimum_Number_of_Coins(Bottom-Up).cpp
@@ -1,33 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_AMOUNT = 1000;
-const int MAX_COINS = 100;
-
-int coins[MAX_COINS];
-int dp[MAX_AMOUNT + 1];
-
-int minCoinsBU(int n, int amount) {
-    for (int i = 0; i <= amount; i++)
-        dp[i] = INT_MAX;
+int minCoinsBU(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, INT_MAX);
     dp[0] = 0;
 
     for (int amt = 1; amt <= amount; amt++) {
-        for (int i = 0; i < n; i++) {
-            if (coins[i] <= amt && dp[amt - coins[i]] != INT_MAX)
-                dp[amt] = min(dp[amt], 1 + dp[amt - coins[i]]);
+        for (int coin : coins) {
+            if (coin <= amt && dp[amt - coin] != INT_MAX)
+                dp[amt] = min(dp[amt], 1 + dp[amt - coin]);
         }
     }
-
     return dp[amount] == INT_MAX ? -1 : dp[amount];
 }
 
 int main() {
-    int n = 3;
-    coins[0] = 1; coins[1] = 2; coins[2] = 5;
+    vector<int> coins = {1, 2, 5};
     int amount = 11;
 
-    cout << "Min coins (Bottom-Up): " << minCoinsBU(n, amount) << endl;
+    cout << "Min coins (Bottom-Up): " << minCoinsBU(coins, amount) << endl;
     return 0;
 }
-
