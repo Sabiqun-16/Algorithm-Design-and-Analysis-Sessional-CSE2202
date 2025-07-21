9a0a7198e1324e555bf582f16a2d92dd33e8c582
Sabiqunnahar <sabiqunnahar65@gmail.com>
2025-07-22 00:21:18 +0600

    Update CoinChange-Total_Number_of_Ways(Bottom-Up).cpp

--- Dynamic Programme/Bottom-Up/CoinChange-Total_Number_of_Ways(Bottom-Up).cpp
+++ Dynamic Programme/Bottom-Up/CoinChange-Total_Number_of_Ways(Bottom-Up).cpp
@@ -1,15 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX_COINS = 100;
-const int MAX_AMOUNT = 1000;
+int countWaysBU(vector<int>& coins, int amount) {
+    int n = coins.size();
+    vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
 
-int dp[MAX_COINS + 1][MAX_AMOUNT + 1];
-int coins[MAX_COINS];
-
-int countWaysBU(int n, int amount) {
-    for (int i = 0; i <= n; i++)
-        dp[i][0] = 1;
+    for (int i = 0; i <= n; i++) dp[i][0] = 1;
 
     for (int i = 1; i <= n; i++) {
         for (int amt = 0; amt <= amount; amt++) {
@@ -19,16 +15,13 @@ int countWaysBU(int n, int amount) {
                 dp[i][amt] = dp[i - 1][amt];
         }
     }
-
     return dp[n][amount];
 }
 
 int main() {
-    int n = 3;
-    coins[0] = 1; coins[1] = 2; coins[2] = 5;
+    vector<int> coins = {1, 2, 5};
     int amount = 5;
 
-    cout << "Total ways (Bottom-Up): " << countWaysBU(n, amount) << endl;
+    cout << "Total ways (Bottom-Up): " << countWaysBU(coins, amount) << endl;
     return 0;
 }
-
